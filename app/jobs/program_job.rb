1f873e7983b3ea0b30bbce741824645271a00c95
cgtuttle <cgtuttle@gmail.com>
2016-08-13 21:28:07 -0500

    Sucker Punch classes

--- app/jobs/program_job.rb
+++ app/jobs/program_job.rb
@@ -3,7 +3,7 @@ class ProgramJob
 
   def perform(program)
   	while true do
-  		if program.start_at >= Time.now && program.last_run_on < program.start_at
+  		if (program.start_at >= Time.now) && (program.last_run_on < program.start_at)
   			program.last_run_on = program.start_at
   			program.save
   			StationJob.perform(program)
