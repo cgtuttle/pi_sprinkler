f3d4c3e482b331567846c0c4fe67dacce4557111
cgtuttle <cgtuttle@gmail.com>
2016-08-14 08:40:09 -0500

    Testing Sucker Punch classes

--- app/jobs/station_job.rb
+++ app/jobs/station_job.rb
@@ -8,7 +8,7 @@ class StationJob
 	  	@stations.each do |program_station|
 				gpio = program_station.port.gpio
 				pin = Pin.from_gpio(gpio)
-	  		pin.value(Time.now() between?(program_station.start_at, program_station.stop_at) ? 1 : 0)
+	  		pin.value(Time.now().between?(program_station.start_at, program_station.stop_at) ? 1 : 0)
 	  	end
 	  end
   end
