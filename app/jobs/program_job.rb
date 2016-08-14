0947e6730a565b5dc0180bba46534366bd6624c4
cgtuttle <cgtuttle@gmail.com>
2016-08-14 15:23:55 -0500

    Debugging ProgramJob

--- app/jobs/program_job.rb
+++ app/jobs/program_job.rb
@@ -6,6 +6,7 @@ class ProgramJob
   	last_run = program.last_run_on || Date.new(1900,1,1)
   	next_run = program.next_date + program.start_seconds
   	while $is_running do
+  		puts "last_run_on = #{program.last_run_on}, next_run = #{program.next_run}, run_now? = #{program.run_now?}"
   		if (program.run_now?) && (program.last_run_on < next_run)
   			puts "Initiating StationJob"
   			program.last_run_on = next_run
