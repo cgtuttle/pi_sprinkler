9df7f60b0b7547efa5530c1d5efe8efb09603af3
cgtuttle <cgtuttle@gmail.com>
2016-08-14 14:38:16 -0500

    Debugging next_date

--- app/models/program.rb
+++ app/models/program.rb
@@ -8,11 +8,12 @@ class Program < ActiveRecord::Base
 	accepts_nested_attributes_for :program_stations
 
 	def next_date
-		now = Time.now
 		rule_met = false
-		date_next = now.to_date + (now < (now.to_date + self.start_time.seconds_since_midnight.seconds + self.total_duration*60) ? 0 : 1)
-		puts "now = #{now}, start_time = #{self.start_time}, duration = #{self.total_duration}, date_next = #{date_next}"
-		until rule_met || date_next == (now.to_date + 365) do
+		time_now = Time.now.seconds_since_midnight.seconds
+		date_now = Date.today()
+		date_offset = time_now < self.stop_seconds ? 0 : 1
+		date_next = date_now + date_offset
+		until rule_met || date_next == (date_now + 365) do
 			self.rules.each do |rule|
 				rule_met = rule.is_valid?(date_next)
 				break if rule_met
@@ -21,6 +22,14 @@ class Program < ActiveRecord::Base
 			date_next += 1
 		end
 		date_return
+	end
+
+	def start_seconds
+		self.start_time.seconds_since_midnight.seconds
+	end
+
+	def stop_seconds
+		self.start_seconds + self.total_duration*60.seconds
 	end
 
 	def start_at
