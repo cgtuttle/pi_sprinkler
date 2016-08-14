97ef9be30713d2d8c15ffe223c08dc3b4fa6639b
cgtuttle <cgtuttle@gmail.com>
2016-08-14 12:43:20 -0500

    Debugged scheduler

--- app/jobs/program_job.rb
+++ app/jobs/program_job.rb
@@ -6,8 +6,8 @@ class ProgramJob
   	last_run = program.last_run_on || Date.new(1900,1,1)
   	start_at = program.start_at
   	while $is_running do
-  		puts "is_runing = #{$is_running}"
-  		if (start_at >= Time.now) && (last_run < start_at)
+  		puts "Running"
+  		if (start_at >= Time.now) && (last_run <= start_at)
   			program.last_run_on = start_at
   			program.save
   			StationJob.perform_async(program)
